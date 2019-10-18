2d08485c0a51bb60d628912740861e69c888b1c9
ASValles <selene_kitty23@hotmail.com>
2019-10-18 01:22:26 -0500

    agregar modelos al canvas y camara

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -3,6 +3,7 @@
 
 <head>
 
+  <title>GameJap</title>
   
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -10,61 +11,27 @@
   <meta name="author" content="">
   <script type="text/javascript" src="jquery.js"></script>
   <script type="text/javascript" src="jquery/jquery-2.1.4.min.js"></script>
-	<script type="text/javascript" src="three/three.js"></script>
+  <script type="text/javascript" src="three/three.js"></script>
+	<script type="text/javascript" src="three/MTLLoader.js"></script>
+	<script type="text/javascript" src="three/OBJLoader.js"></script>
   <script src="{{ asset('vendor/sweetalert/sweetalert.all.js') }}"></script>
   
-  <title>GameJap</title>
   
   <!-- Font Awesome Icons -->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
   <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
-  
   <!-- Plugin CSS -->
   <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
-  
   <!-- Theme CSS - Includes Bootstrap -->
   <link href="css/creative.min.css" rel="stylesheet">
-  
   <!-- Bootstrap Core CSS -->
   <link href="vendor/bootstrap.min.css" rel="stylesheet">
-  
   <!-- Custom CSS -->
   <link href="css/stylish-portfolio.min.css" rel="stylesheet">
   
-  <!--<script type="text/javascript">
-  function cargaContextoCanvas(micanvas){
-    var elemento = document.getElementById(micanvas);
-    if(elemento && elemento.getContext){
-      var contexto = elemento.getContext('2d');
-      if(contexto){
-        return contexto;
-      }
-    }
-    return FALSE;
-  }
-  
-  
-  window.onload = function(){
-    //Recibimos el elemento canvas
-    var ctx = cargaContextoCanvas('micanvas');
-    if(ctx){
-      //Creo una imagen conun objeto Image de Javascript
-      var img = new Image();
-      //indico la URL de la imagen
-      img.src = 'img1.png';
-      //defino el evento onload del objeto imagen
-      img.onload = function(){
-        //incluyo la imagen en el canvas
-        ctx.drawImage(img, 10, 10);
-      }
-        }
-      }
-    </script>-->
-    
-    <script type="text/javascript">
+  <script type="text/javascript">
       $(document).ready(function() {
 
         $('#btn_play').click(function() {
@@ -123,165 +90,280 @@
     });
   </script>
 
-  <script type="text/javascript">
+      <script type="text/javascript">
+        var scene;
+        var camera;
+        var renderer;
+        var controls;
+        var objects = [];
+        var clock;
+        var deltaTime;	
+        var keys = {};
 
-  //se divide en 3 componentes basicos
+        var persona;
 
-  //COLECCION DE OBJETOS THREEJS
-  var scene;  //lugar donde vamos a guardar en el render, que quieres que aparezca en escena
-  //DIBUJA EN ESCENA
-  var renderer; //dibujar lo que esta en escena
+        var raycaster;
+        var objetosConColision = [];
+        
+        var isWorldReady = [ false, false, false, false, false, false, false, false, false, false,
+                              false, false, false, false, false, false, false ];
 
-  //PUNTO DE VISTA
-  var camera;
+        $(document).ready(function() {
 
-  var aux = false;
-  var aux2 = false;
+          clock=new THREE.Clock();
+
+          setupScene();
+
+          //INICIALIZAMOS EL RAYCASTER
+          raycaster= new THREE.Raycaster();
+          camera.misRayos = [
+            new THREE.Vector3(0,0,1),
+            new THREE.Vector3(0,0,-1),
+            new THREE.Vector3(1,0,0),
+            new THREE.Vector3(-1,0,0)
+          ];
+
+          loadOBJWithMTL("assets/", "Terreno2.obj", "Terreno2.mtl", (objetoCargado) => {
+            objetoCargado.position.z = -1;
+              //var objetoCargado2= objetoCargado.clone();
+              //objetoCargado2.position.z=50;              
+              scene.add(objetoCargado);
+              //scene.add(objetoCargado2);
+              //objetosConColision.push(objetoCargado);
+              //objetosConColision.push(objetoCargado2);
+            
+            isWorldReady[0] = true;
+          });
+
+          loadOBJWithMTL("assets/", "Templo2.obj", "Templo2.mtl", (templo) => {
+            templo.position.z = -1;
+            scene.add(templo);
+            isWorldReady[1] = true;
+          });
+
+          loadOBJWithMTL("assets/", "Personaje.obj", "Personaje.mtl", (personaje) => {
+            personaje.position.z = -30;
+            personaje.position.x = -23;
+            personaje.rotation.y = THREE.Math.degToRad(180);
+            //scene.add(personaje);
+            persona.add(personaje);
+            isWorldReady[2] = true;
+          });
+
+          loadOBJWithMTL("assets/", "Arena2.obj", "Arena2.mtl", (arena) => {
+            arena.position.z = -1;
+            scene.add(arena);
+            isWorldReady[3] = true;
+          });
+
+          loadOBJWithMTL("assets/", "Casa.obj", "Casa.mtl", (casa) => {
+            casa.position.z = -1;
+            scene.add(casa);
+            isWorldReady[4] = true;
+          });
+
+          loadOBJWithMTL("assets/", "CasaMayor.obj", "CasaMayor.mtl", (casaMayor) => {
+            casaMayor.position.z = -1;
+            scene.add(casaMayor);
+            isWorldReady[5] = true;
+          });
+
+          loadOBJWithMTL("assets/", "CasasMenores.obj", "CasasMenores.mtl", (casasMenores) => {
+            casasMenores.position.z = -1;
+            scene.add(casasMenores);
+            isWorldReady[6] = true;
+          });
+
+          loadOBJWithMTL("assets/", "Farol.obj", "Farol.mtl", (farol) => {
+            farol.position.z = -1;
+            scene.add(farol);
+            isWorldReady[7] = true;
+          });
+
+          loadOBJWithMTL("assets/", "Ficha.obj", "Ficha.mtl", (ficha) => {
+            ficha.position.z = -1;
+            scene.add(ficha);
+            isWorldReady[8] = true;
+          });
+
+          loadOBJWithMTL("assets/", "Mascara1.obj", "Mascara1.mtl", (mask1) => {
+            mask1.position.z = -1;
+            scene.add(mask1);
+            isWorldReady[9] = true;
+          });
+
+          loadOBJWithMTL("assets/", "Mascara2.obj", "Mascara2.mtl", (mask2) => {
+            mask2.position.z = -1;
+            scene.add(mask2);
+            isWorldReady[10] = true;
+          });
+
+          loadOBJWithMTL("assets/", "Puente.obj", "Puente.mtl", (puente) => {
+            puente.position.z = -1;
+            scene.add(puente);
+            isWorldReady[11] = true;
+          });
+
+          loadOBJWithMTL("assets/", "RegaMayor.obj", "RegaMayor.mtl", (rejaMayor) => {
+            rejaMayor.position.z = -1;
+            scene.add(rejaMayor);
+            isWorldReady[12] = true;
+          });
+
+          loadOBJWithMTL("assets/", "Roca1.obj", "Roca1.mtl", (roca1) => {
+            roca1.position.z = -1;
+            scene.add(roca1);
+            isWorldReady[13] = true;
+          });
+
+          loadOBJWithMTL("assets/", "Roca2.obj", "Roca2.mtl", (roca2) => {
+            roca2.position.z = -1;
+            scene.add(roca2);
+            isWorldReady[14] = true;
+          });
+
+          loadOBJWithMTL("assets/", "Roca3.obj", "Roca3.mtl", (roca3) => {
+            roca3.position.z = -1;
+            scene.add(roca3);
+            isWorldReady[15] = true;
+          });
+
+          loadOBJWithMTL("assets/", "Roca4.obj", "Roca4.mtl", (roca4) => {
+            roca4.position.z = -1;
+            scene.add(roca4);
+            isWorldReady[16] = true;
+          });
 
 
-  $(document).ready(function() {
+          scene.add(persona);
+          camera.position.z = 8;
+          camera.position.y = 14;
+          //var grados = THREE.Math.degToRad(10);
+          //camera.rotation.x = -grados;
+          persona.add(camera);
 
-    var tamanoCanvas = {
-      width: 1550,
-      height: 800
+          render();
 
-    }
+          document.addEventListener('keydown', onKeyDown);
+          document.addEventListener('keyup', onKeyUp);		
+        });
 
-    //Inicializamos el renderer
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(
-      tamanoCanvas.width, 
-      tamanoCanvas.height
-    );
-    renderer.setClearColor( new THREE.Color(0.5, 0, 0.5) );  //tmb se puede colores exadecimales ("")
+        function loadOBJWithMTL(path, objFile, mtlFile, onLoadCallback) {
+          var mtlLoader = new THREE.MTLLoader();
+          mtlLoader.setPath(path);
+          mtlLoader.load(mtlFile, (materials) => {
+            
+            var objLoader = new THREE.OBJLoader();
+            objLoader.setMaterials(materials);
+            objLoader.setPath(path);
+            objLoader.load(objFile, (objetoCargado) => {
+              onLoadCallback(objetoCargado);
+            });
 
-    //Incializamos la camara
-    camera = new THREE.PerspectiveCamera(
-        75, //apertura de camara
-        tamanoCanvas.width / tamanoCanvas.height,
-        0.1, //que tan cercas para que se dibuje
-        100  //que tan lejos para que se dibuje por el render
-      );
+          });
+        }
 
-    //Inicializamos la escena
-    scene = new THREE.Scene();
+        function onKeyDown(event) {
+          keys[String.fromCharCode(event.keyCode)] = true;
+        }
+        function onKeyUp(event) {
+          keys[String.fromCharCode(event.keyCode)] = false;
+        }
 
-    //Agregamos el canvas
-    $('#can').append( renderer.domElement );
+        
+        function render() {
+          requestAnimationFrame(render);
+          var bool=false;
+          deltaTime = clock.getDelta();	
 
-    ///////////////////////////////////////////////////////// OBJETOS ////////////////////////////////
+          var yaw = 0;
+          var forward = 0;
+          if (keys["A"]) {
+            yaw = 5;
+          } else if (keys["D"]) {
+            yaw = -5;
+          }
+          if (keys["W"]) {
+            forward = -20;
+          } else if (keys["S"]) {
+            forward = 20;
+          }
 
-    /*LA CLASE BASE DE LOS OBJETOS DE THREEJS
-      -Object3D
-        -position.x
-        -scale.x
-        -rotation.x
+          
 
-      arreglo por cada objeto osease geometria
-    */
+          if (isWorldReady[0] && isWorldReady[1]) {
 
-    // 1.-Info del objeto (vertices, indices, coordenadas, etc..)
-    var geometry= new THREE.BoxGeometry(1, 1, 1); //ancho,alto y profundidad
+            persona.translateZ(forward * deltaTime);
 
-    // 2.- Material del objeto, define como se vera
-    var material = new THREE.MeshLambertMaterial({   //Basico
-      color: new THREE.Color(0.7, 0.0, 0.0)
-    });
+            for(var i = 0; i < camera.misRayos.length; i++){
+              var rayo = camera.misRayos[i];
 
-    // 3.- Mesh
-    var cube = new THREE.Mesh( geometry, material);
+              raycaster.set( camera.position, rayo );
 
-    camera.position.z = 2;
-    //camera.position.set(0,0,2); con este afectamos los 3 ejes al mismo tiempo
+              var colision = raycaster.intersectObjects(
+                objetosConColision,
+                true
+              );
 
-    scene.add( cube );
+              if( colision.length > 0 ){
+                if(colision[0].distance < 15){
+                //Si hay colision
+                  console.log("Estoy colisionando");
+                  //bool = true;
+                  //colision[0].object.parent.rotar = true
+                  //raycast
+                  //colision[0]
+                }
+              }
+            }
 
-    var material2 = new THREE.MeshPhongMaterial({ //refleja totalmente la luz, contrario de lambert
-      color: new THREE.Color(0.5, 0.5, 0.5),
-      specular: new THREE.Color(1, 1, 1),
-      shininess: 500
-    });
+            persona.rotation.y += yaw * deltaTime;
+          }
+          
+          //if(bool == true)    for( i<objetosConColision.leght i++)
+          //{				if(objectosconColision{i}.rotar === true)
+          //	coli.rotation.z += THREE.Math.degToRad(50)*deltaTime;  .rotario.z += 0.001
+          //}
+        
+          //personaje.rotation.y += yaw * deltaTime;
+		      //personaje.translateZ( forward * deltaTime );
+          //camera.lookAt( personaje.position );
+          
+          renderer.render(scene, camera);
+        }
 
-    var cube2 = new THREE.Mesh( geometry, material2);
-    cube2.position.x = 1
-    cube.position.x = -1
+        function setupScene() {		
+          //var visibleSize = { width: window.innerWidth, height: window.innerHeight};
+          var visibleSize = { width: 1500, height: 800};
+          clock = new THREE.Clock();		
+          scene = new THREE.Scene();
+          persona = new THREE.Object3D();
+          camera = new THREE.PerspectiveCamera(75, visibleSize.width / visibleSize.height, 0.1, 1000);
+          camera.position.z = 2;
+          camera.position.y = 10;
 
-    scene.add( cube2 );
+          renderer = new THREE.WebGLRenderer( {precision: "mediump" } );
+          renderer.setClearColor(new THREE.Color(0, 0, 0));
+          renderer.setPixelRatio(visibleSize.width / visibleSize.height);
+          renderer.setSize(visibleSize.width, visibleSize.height);
 
-    //Iluminacion
+          var ambientLight = new THREE.AmbientLight(new THREE.Color(1, 1, 1), 1.0);
+          scene.add(ambientLight);
 
-    var ambient = new THREE.AmbientLight(
-        new THREE.Color(1, 1, 1),  //parametro de luz
-        1.0  //intencidad
-      );
+          var directionalLight = new THREE.DirectionalLight(new THREE.Color(1, 1, 0), 0.4);
+          directionalLight.position.set(0, 0, 1);
+          scene.add(directionalLight);
 
-    var directional = new THREE.DirectionalLight(  //directonal
-        new THREE.Color(1, 1, 0),
-        0.4
-      );
+          var grid = new THREE.GridHelper(50, 10, 0xffffff, 0xffffff);
+          grid.position.y = -1;
+          scene.add(grid);
+          
+          
 
-    directional.position.set(0, 0, 1);
+          $('#can').append(renderer.domElement);
+        }
 
-    scene.add( ambient );
-    scene.add( directional );
-
-    cube.name = "cubo1";
-    cube2.name = "cubo2";
-
-
-    render();
-  });
-
-  function render() { //SE ESTA LLAMANDO SIEMPRE, aqui logica de colisiones, animaciones, controles etc
-
-    var cubo1 = scene.getObjectByName('cubo1');
-    var cubo2 = scene.getObjectByName('cubo2');
-
-    //cubo1.rotation.y += THREE.Math.degToRad(1);
-    //cubo2.rotation.y -= THREE.Math.degToRad(1);
-
-    if(cubo1.position.x >= 1.5 && !aux)
-    {
-      aux = true;
-    }
-    if(cubo1.position.x <= -1.5 && aux)
-    {
-      aux = false;
-    }
-
-    if(!aux)
-    {
-      cubo1.position.x += .05
-    }
-    else
-    {
-      cubo1.position.x -= .05
-    }
-
-
-    if(cubo2.position.x <= -1.5 && !aux2)
-    {
-      aux2 = true;
-    }
-    if(cubo2.position.x >= 1.5 && aux2)
-    {
-      aux2 = false;
-    }
-
-    if(!aux2)
-    {
-      cubo2.position.x -= .05
-    }
-    else
-    {
-      cubo2.position.x += .05
-    }
-
-    renderer.render( scene, camera);
-
-    requestAnimationFrame(render); //recive el parametro y hace ciclo
-  }
-  </script>
+        </script>
   
 
 </head>
