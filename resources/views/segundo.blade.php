affa6b5cae031427948481044483934740eb4cd9
ASValles <selene_kitty23@hotmail.com>
2019-11-19 00:09:13 -0600

    facebook 14

--- resources/views/segundo.blade.php
+++ resources/views/segundo.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Screen</title>
+    <script type="text/javascript" src="three/mifacebook.js"></script>
 
     <script type="text/javascript">
         function shareFB(){
