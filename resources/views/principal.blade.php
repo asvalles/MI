260f8087ae557d034e4c6b7222678e130466c168
ASValles <selene_kitty23@hotmail.com>
2019-11-19 21:58:25 -0600

    musica 2

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -3070,8 +3070,13 @@
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4"><div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
           <br><br><h1 style="font-size: 50px">GameJap</h1>
                         <center>
-                        <audio autoplay loop>
-                              <source src="music/naruto.mp3"></audio>
+                        <audio autoplay loop id="audioNaruto">
+                          <source src="music/naruto.mp3">
+                        </audio>
+                        <script> 
+                          var music = document.getElementById("audioNaruto");
+                          music.volume = 0.6;
+                        </script>
 
                           <div id="NivelUno_Texto" style="display: none">
                             <h3>Palabra a recolectar: No<br>Hitokoto: いいえ (iie)<br></h3>
